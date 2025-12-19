d4419d852c6d17d842b7b9e4b36cfcfac85ac942
Stefan.G <stefangudalovic@gmail.com>
2025-12-19 08:28:32 +0100

    simple GUI

--- build.sbt
+++ build.sbt
@@ -4,6 +4,7 @@ ThisBuild / scalaVersion := "3.3.7"
 
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.19" % "test"
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.19"
+libraryDependencies += "org.scala-lang.modules" %% "scala-swing" % "3.0.0"
 
 lazy val root = (project in file("."))
   .settings(
--- src/main/scala/de/htwg/se/Roulette/aview/ConsoleObserver.scala
+++ src/main/scala/de/htwg/se/Roulette/aview/ConsoleObserver.scala
@@ -1,18 +1,30 @@
 package de.htwg.se.Roulette.aview
 
 import de.htwg.se.Roulette.controller._
+import de.htwg.se.Roulette.model.PrintTable
 
 class ConsoleObserver(controller: GameController) extends Observer[ControllerEvent] {
   controller.addObserver(this)
-  override def update(event: ControllerEvent): Unit =
+  override def update(event: ControllerEvent): Unit = {
     event match {
-      case NewRound(_) =>
-        println(s"ConsoleObserver: New round started. ")
-//        println(s"ConsoleObserver: Winning number: ${gs.winningNumber}")
+      case NewRound(gs) =>
+        println("New round started.")
+        println(PrintTable.printTable(11, 1, None))
+        println("Place your bet(s) (e.g., '10 r 1/3'), or type 'new', 'undo', 'quit'.")
       case BetPlaced(gs) =>
-        println(s"ConsoleObserver: Bets placed: ${gs.bets.mkString(", ")} on result ${gs.winningNumber}")
+        println(s"Bets placed: ${gs.bets.mkString(", ")}")
+        gs.bets.foreach { bet =>
+          if (bet.isWinningBet(gs.winningNumber)) {
+            println(s"You WON on your bet: $bet")
+          } else {
+            println(s"You LOST on your bet: $bet")
+          }
+        }
+        println(PrintTable.printTable(11, 1, Some(gs.winningNumber)))
+        println("Type 'new' to start a new round.")
       case BetUndone =>
-        println("ConsoleObserver: Last round was undone.")
-      case e => println(s"ConsoleObserver received event: $e")
+        println("Last bet was undone. You can place a new bet.")
+        println(PrintTable.printTable(11, 1, None))
     }
+  }
 }
